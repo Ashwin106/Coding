a5832dafafc8ff353684d62ebb7f1c270009fb00
ashwina <ashwinkrishna815@gmail.com>
2025-10-10 13:16:56 +0530

    practice strings/remove duplicates from string

--- src/main/java/Practice_strings/remove_duplicates_from_string.java
+++ src/main/java/Practice_strings/remove_duplicates_from_string.java
@@ -1,19 +1,42 @@
 package Practice_strings;
 
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class remove_duplicates_from_string {
     public static void main(String[]args){
         String input="Programming";
-        Set<Character> set=new HashSet<>();
+        Set<Character> unique=new HashSet<>();
+        Set<Character> duplicate=new HashSet<>();
+        Map<Character, Integer> freqMap = new LinkedHashMap<>();
+
         for(char ch:input.toCharArray()){
-            set.add(ch);
+            if(!unique.add(ch)){
+                freqMap.put(ch,freqMap.getOrDefault(ch,0)+1);
+                duplicate.add(ch);
+            }
+            else{
+                freqMap.put(ch,1);
+            }
+            //unique.add(ch)
         }
         StringBuilder sb=new StringBuilder();
-        for(char c:set){
+        StringBuilder sb1=new StringBuilder();
+        for(char c:unique){
             sb.append(c);
         }
-        System.out.println("without duplicates: "+sb.toString() );
+        for(char d:duplicate){
+            sb1.append(d);
+        }
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " -> " + entry.getValue());
+                //hasDuplicates = true;
+            }
+        }
+        System.out.println(" unique : "+sb.toString() );
+        System.out.println("duplicates:" +sb1.toString());
     }
 }
